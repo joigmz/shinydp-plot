0b940e91f7937096425c95bc72165fe77fd57800
joigmz <jose.izam99@gmail.com>
2022-01-23 23:22:35 -0300

    avance 01-23

--- server.R
+++ server.R
@@ -5,11 +5,25 @@ library(ggplot2)
 library(dplyr)
 library(data.table)
 library(scales)
+library(DT)
+
 
 data <- fread("/Users/joseizammontt/Desktop/Universidad/master/thesis/codigo/DATA_UNV_2.csv", header=TRUE, stringsAsFactors=FALSE)
 data <- data[F_UNIDADESVENTA>0.000001,]
 data <- data[,WEEK:=strftime(as.POSIXlt(data$ID_DIAANALISIS, format = "%Y-%d-%m"), "%W")]
 data <- data[,.(WEEK,DESC_LOCALFISICO,COD_ZONAL,DIVISION,DEPARTAMENTO,SUBDEPARTAMENTO,CLASE,SUBCLASE,COD_SKU,DESC_SKU,F_UNIDADESVENTA,F_MONTOVENTA,F_COSTOVENTA)]
+
+theme_clean <- function() {
+  theme_minimal(base_family = "Barlow Semi Condensed") +
+    theme(panel.grid.minor = element_blank(),
+          plot.background = element_rect(fill = "white", color = NA),
+          plot.title = element_text(face = "bold", size=24),
+          axis.title = element_text(face = "bold", size=20),
+          text = element_text(size=20),
+          strip.text = element_text(face = "bold", size = rel(0.8), hjust = 0),
+          strip.background = element_rect(fill = "grey80", color = NA),
+          legend.title = element_text(face = "bold"))
+}
 
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
@@ -17,6 +31,7 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     
     data.grafico1 <-data[DESC_LOCALFISICO==input$selectInput2 & CLASE==input$selectInput1,]
+    data.grafico1 <-data.grafico1[WEEK>=input$from_date & WEEK<=input$to_date,]
     data.grafico1 <- data.grafico1[,.(MONTOVENTAS = sum(F_MONTOVENTA), DEMANDA = sum(F_UNIDADESVENTA)), keyby = .(WEEK,DESC_LOCALFISICO,CLASE,COD_SKU,DESC_SKU)]
     data.grafico1 <- data.grafico1[,PRECIOPROMEDIO:=data.grafico1$MONTOVENTAS/data.grafico1$DEMANDA]
     data.grafico1 <- data.grafico1[DESC_SKU==input$selectInput3,]
@@ -33,32 +48,108 @@ shinyServer(function(input, output) {
     
     # now adding the secondary axis, following the example in the help file ?scale_y_continuous
     # and, very important, reverting the above transformation
-    p <- p + scale_y_continuous(sec.axis = sec_axis(~.*mean(y1)/mean(y2), name = "Precio",labels=scales::dollar_format()))
+    p <- p + scale_y_continuous(sec.axis = sec_axis(~.*mean(y1)/mean(y2), name = "Precio",labels=scales::dollar_format(prefix="S/ ")))
     
     # modifying colours and theme options
     p <- p + scale_colour_manual(values = c("blue", "red"))
     p <- p + labs(y = "Demanda",
-                  x = "Week",
+                  x = " Nº Semana",
                   colour = "Curvas")
+    p <- p + ggtitle('Variación del precio y demanda por semana')
+    p <- p + theme_clean()
     p <- p + theme(legend.position = c(0.85, 0.2))
     p
-    
   })
   
   output$distPlot2 <- renderPlot({
     
     data.grafico2 <-data[DESC_LOCALFISICO==input$selectInput2 & CLASE==input$selectInput1,]
+    data.grafico2 <-data.grafico2[WEEK>=input$from_date & WEEK<=input$to_date,]
     data.grafico2 <- data.grafico2[DESC_SKU==input$selectInput3,]
     data.grafico2 <- data.grafico2[,PRECIOVENTA:=data.grafico2$F_MONTOVENTA/data.grafico2$F_UNIDADESVENTA]
     
     p2 <- ggplot(data.grafico2, aes(x=WEEK, y=PRECIOVENTA)) + 
       geom_boxplot()+
-      scale_y_continuous(labels=scales::dollar_format())
+      scale_y_continuous(labels=scales::dollar_format(prefix="S/ "))
+    p2 <- p2 + labs(y = "Precio Venta",
+                  x = " Nº Semana",
+                  colour = "Curvas")
+    p2 <- p2 + ggtitle('Variación del precio por semana')
+    
+    p2 <- p2 + theme_clean()
     p2
   })
   
+  output$Demand_summary = DT::renderDataTable({
+    
+    data.grafico2 <-data[DESC_LOCALFISICO==input$selectInput2 & CLASE==input$selectInput1,]
+    data.grafico2 <-data.grafico2[WEEK>=input$from_date & WEEK<=input$to_date,]
+    data.grafico2 <- data.grafico2[DESC_SKU==input$selectInput3,]
+    data.grafico2 <- data.grafico2[,PRECIOVENTA:=data.grafico2$F_MONTOVENTA/data.grafico2$F_UNIDADESVENTA]
+    
+    df_grafico2 <- data_frame(data.grafico2)
+    
+    price_summary <- c()
+    weeks_list <- sort(unique(df_grafico2$WEEK))
+    for (week_number in weeks_list){
+      sold_units <-df_grafico2[df_grafico2$WEEK==as.character(week_number),c('F_UNIDADESVENTA')]
+      price_summary_aux <- transpose(data.table(do.call(cbind, lapply(sold_units, summary))))
+      price_summary <- rbind(price_summary,price_summary_aux)
+    }
+    
+    price_summary <- cbind(weeks_list,price_summary)
+    colnames(price_summary) <- c('Week','Min','1st Qu','Median','Mean','3rd Qu','Max')
+    price_summary[,c('Min','1st Qu','Median','Mean','3rd Qu','Max')]<- round(price_summary[,c('Min','1st Qu','Median','Mean','3rd Qu','Max')] ,digit=2)
+    price_summary
+  })
+
+  output$price_summary = DT::renderDataTable({
+    
+    data.grafico2 <-data[DESC_LOCALFISICO==input$selectInput2 & CLASE==input$selectInput1,]
+    data.grafico2 <-data.grafico2[WEEK>=input$from_date & WEEK<=input$to_date,]
+    data.grafico2 <- data.grafico2[DESC_SKU==input$selectInput3,]
+    data.grafico2 <- data.grafico2[,PRECIOVENTA:=data.grafico2$F_MONTOVENTA/data.grafico2$F_UNIDADESVENTA]
+    
+    df_grafico2 <- data_frame(data.grafico2)
+    
+    price_summary <- c()
+    weeks_list <- sort(unique(df_grafico2$WEEK))
+    for (week_number in weeks_list){
+      sold_units <-df_grafico2[df_grafico2$WEEK==as.character(week_number),c('PRECIOVENTA')]
+      price_summary_aux <- transpose(data.table(do.call(cbind, lapply(sold_units, summary))))
+      price_summary <- rbind(price_summary,price_summary_aux)
+    }
+    
+    price_summary <- cbind(weeks_list,price_summary)
+    colnames(price_summary) <- c('Week','Min','1st Qu','Median','Mean','3rd Qu','Max')
+    price_summary[,c('Min','1st Qu','Median','Mean','3rd Qu','Max')]<- round(price_summary[,c('Min','1st Qu','Median','Mean','3rd Qu','Max')] ,digit=2)
+    price_summary
+  })
+  output$Zone_Ranking <- renderPlot({
+    if (input$qzonal=="Zonal"){
+      ranking_zonal <- data[,.(MONTOVENTAS = sum(F_MONTOVENTA)), keyby = .(COD_ZONAL)]
+      ranking_zonal <- setorder(ranking_zonal,-MONTOVENTAS)
+      
+      ggplot(ranking_zonal, aes(x = reorder(COD_ZONAL, MONTOVENTAS), y = MONTOVENTAS)) +
+        geom_bar(stat = "identity",
+                 show.legend = FALSE,
+                 fill= "lightblue",
+                 color = "white") +
+        geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
+                      hjust = 0,
+                      vjust = 0.5),
+                  size = 5)+
+        xlab("Zona") +
+        ylab("Ventas totales") +
+        ggtitle('Ranking de ventas por zona')+
+        coord_flip()+
+        scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(ranking_zonal$MONTOVENTAS)*1.3))+
+        theme_clean()
+    }
+  })
+  
   output$distPlot3 <- renderPlot({
-    if (input$qzonal=="Si"){
+    if (input$qzonal=="Zonal"){
     data.grafico3 <- data[COD_ZONAL == input$cod_zonal,]
     data.grafico3 <- data.grafico3[,.(MONTOVENTAS = sum(F_MONTOVENTA)), keyby = .(DESC_LOCALFISICO)]
     data.grafico3 <- setorder(data.grafico3,-MONTOVENTAS)
@@ -68,14 +159,16 @@ shinyServer(function(input, output) {
                show.legend = FALSE,
                fill= "lightblue",
                color = "white") +
-      geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+      geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                     hjust = 0,
                     vjust = 0.5),
                 size = 5)+
-      xlab("LOCAL") +
-      ylab("MONTOVENTAS") +
+      xlab("Local") +
+      ylab("Ventas totales") +
+      ggtitle(paste('Detalle ventas zona',input$cod_zonal))+
       coord_flip()+
-      scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico3$MONTOVENTAS)*1.3))
+      scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico3$MONTOVENTAS)*1.3))+
+      theme_clean()
     } else {
     data.grafico3 <- data[,.(MONTOVENTAS = sum(F_MONTOVENTA)), keyby = .(DESC_LOCALFISICO)]
     data.grafico3 <- setorder(data.grafico3,-MONTOVENTAS)
@@ -87,14 +180,16 @@ shinyServer(function(input, output) {
                show.legend = FALSE,
                fill= "lightblue",
                color = "white") +
-      geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+      geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                     hjust = 0,
                     vjust = 0.5),
                 size = 5)+
-      xlab("LOCAL") +
-      ylab("MONTOVENTAS") +
+      xlab("Local") +
+      ylab("Ventas totales") +
+      ggtitle(paste('Detalle ventas por local'))+
       coord_flip()+
-      scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico3$MONTOVENTAS)*1.3))
+      scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico3$MONTOVENTAS)*1.3))+
+      theme_clean()
     }
   })
   
@@ -108,14 +203,16 @@ shinyServer(function(input, output) {
                  show.legend = FALSE,
                  fill= "lightblue",
                  color = "white") +
-        geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+        geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                       hjust = 0,
                       vjust = 0.5),
                   size = 5)+
+        ggtitle(paste('Ventas toales a nivel',input$selectInput5))+
         xlab("DIVISION") +
-        ylab("MONTOVENTAS") +
+        ylab("Ventas Totales") +
         coord_flip()+
-        scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))
+        scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))+
+        theme_clean()
     }
     else if(input$selectInput5=="DEPARTAMENTO"){
       data.grafico4 <- data[,.(MONTOVENTAS = sum(F_MONTOVENTA)), keyby = .(DEPARTAMENTO)]
@@ -126,14 +223,16 @@ shinyServer(function(input, output) {
                  show.legend = FALSE,
                  fill= "lightblue",
                  color = "white") +
-        geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+        geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                       hjust = 0,
                       vjust = 0.5),
                   size = 5)+
+        ggtitle(paste('Ventas toales a nivel',input$selectInput5))+
         xlab("DEPARTAMENTO") +
-        ylab("MONTOVENTAS") +
+        ylab("Ventas Totales") +
         coord_flip()+
-        scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))
+        scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))+
+        theme_clean()
     }
     else if(input$selectInput5=="SUBDEPARTAMENTO"){
       data.grafico4 <- data[,.(MONTOVENTAS = sum(F_MONTOVENTA)), keyby = .(SUBDEPARTAMENTO)]
@@ -144,14 +243,16 @@ shinyServer(function(input, output) {
                  show.legend = FALSE,
                  fill= "lightblue",
                  color = "white") +
-        geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+        geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                       hjust = 0,
                       vjust = 0.5),
                   size = 5)+
+        ggtitle(paste('Ventas toales a nivel',input$selectInput5))+
         xlab("SUBDEPARTAMENTO") +
-        ylab("MONTOVENTAS") +
+        ylab("Ventas Totales") +
         coord_flip()+
-        scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))
+        scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))+
+        theme_clean()
     }
     else if(input$selectInput5=="CLASE"){
       data.grafico4 <- data[,.(MONTOVENTAS = sum(F_MONTOVENTA)), keyby = .(CLASE)]
@@ -163,14 +264,16 @@ shinyServer(function(input, output) {
                  show.legend = FALSE,
                  fill= "lightblue",
                  color = "white") +
-        geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+        geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                       hjust = 0,
                       vjust = 0.5),
                   size = 5)+
+        ggtitle(paste('Ventas toales a nivel',input$selectInput5))+
         xlab("CLASE") +
-        ylab("MONTOVENTAS") +
+        ylab("Ventas Totales") +
         coord_flip()+
-        scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))
+        scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))+
+        theme_clean()
     }
     else if(input$selectInput5=="SUBCLASE"){
       data.grafico4 <- data[,.(MONTOVENTAS = sum(F_MONTOVENTA)), keyby = .(SUBCLASE)]
@@ -182,14 +285,16 @@ shinyServer(function(input, output) {
                  show.legend = FALSE,
                  fill= "lightblue",
                  color = "white") +
-        geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+        geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                       hjust = 0,
                       vjust = 0.5),
                   size = 5)+
+        ggtitle(paste('Ventas toales a nivel',input$selectInput5))+
         xlab("SUBCLASE") +
-        ylab("MONTOVENTAS") +
+        ylab("Ventas Totales") +
         coord_flip()+
-        scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))
+        scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico4$MONTOVENTAS)*1.3))+
+        theme_clean()
     }
   })
   
@@ -204,13 +309,14 @@ shinyServer(function(input, output) {
                show.legend = FALSE,
                fill= "lightblue",
                color = "white") +
-      geom_text(aes(label = dollar(round(MONTOVENTAS, 0)),
+      geom_text(aes(label = dollar(round(MONTOVENTAS, 0), prefix = "S/ "),
                     hjust = 0,
                     vjust = 0.5),
                 size = 5)+
       xlab("DESC_SKU") +
       ylab("MONTOVENTAS") +
       coord_flip()+
-      scale_y_continuous(labels=scales::dollar_format(), limits = c(0, max(data.grafico5$MONTOVENTAS)*1.3))
+      scale_y_continuous(labels=scales::dollar_format(prefix="S/ "), limits = c(0, max(data.grafico5$MONTOVENTAS)*1.3))+
+      theme_clean()
   })
 })
--- ui.R
+++ ui.R
@@ -5,6 +5,7 @@ library(data.table)
 library(flexdashboard)
 library(shinydashboard)
 library(shinythemes)
+library(DT)
 
 data <- fread("/Users/joseizammontt/Desktop/Universidad/master/thesis/codigo/DATA_UNV_2.csv", header=TRUE, stringsAsFactors=FALSE)
 data <- data[F_UNIDADESVENTA>0.000001,]
@@ -12,9 +13,10 @@ data <- data[,WEEK:=strftime(as.POSIXlt(data$ID_DIAANALISIS, format = "%Y-%d-%m"
 data <- data[,.(WEEK,DESC_LOCALFISICO,COD_ZONAL,DIVISION,DEPARTAMENTO,SUBDEPARTAMENTO,CLASE,SUBCLASE,COD_SKU,DESC_SKU,F_UNIDADESVENTA,F_MONTOVENTA,F_COSTOVENTA)]
 
 fluidPage(theme = shinytheme("united"),
+          tags$head(
+            tags$style(HTML('.navbar-static-top {background-color: green; border-color: green;}'))
+          ),
           navbarPage("Tottus Dashboard",
-           tags$head(tags$style(HTML('.navbar-static-top {background-color: green; border-color: green;}',
-                                     '.navbar-default .navbar-nav>.active>a {background-color: green;}'))),
             tabPanel("Gráfico",
               # Sidebar layout with input and output definitions ----
               sidebarLayout(
@@ -34,6 +36,14 @@ fluidPage(theme = shinytheme("united"),
                   selectInput("selectInput3",
                               "Selecionar DESC_SKU:",choices = unique(data$DESC_SKU),
                               selected = "POLLO FRESCO TTN GR X KG"
+                  ),
+                  selectInput("from_date",
+                              "Desde la semana:",choices = sort(unique(data$WEEK)),
+                              selected = min(unique(data$WEEK))
+                  ),
+                  selectInput("to_date",
+                              "Hasta la semana:",choices = sort(unique(data$WEEK)),
+                              selected = max(unique(data$WEEK))
                   )
                   
                 ),
@@ -42,7 +52,12 @@ fluidPage(theme = shinytheme("united"),
                 mainPanel(
                   # Output: Histogram ----
                   plotOutput(outputId = "distPlot",height = "300px"),
-                  plotOutput(outputId = "distPlot2",height = "300px", width = "97%")
+                  tags$hr(),
+                  plotOutput(outputId = "distPlot2",height = "300px", width = "97%"),
+                  h2("Statistical summary of demands by SKU"),
+                  DT::dataTableOutput("Demand_summary"),
+                  h2("Statistical summary of prices by SKU"),
+                  DT::dataTableOutput("price_summary")
                 )
                 
               )
@@ -56,17 +71,18 @@ fluidPage(theme = shinytheme("united"),
                        sidebarPanel(
                          # Input: Slider for the number of bins ----
                          selectInput("qzonal",
-                                     "Analisis por zona:",choices = c("Si","No"),
-                                     selected ="No"
+                                     "Nivel de agregación:",choices = c("Zonal","Local"),
+                                     selected ="Local"
                          ),
                          conditionalPanel(
-                           condition = "input.qzonal == 'Si'",
+                           condition = "input.qzonal == 'Zonal'",
                            selectInput("cod_zonal",
-                                       "Zona:",choices = unique(data$COD_ZONAL)
+                                       "Zona:",choices = unique(data$COD_ZONAL),
+                                       selected = "LM"
                            )
                          ),
                          conditionalPanel(
-                           condition = "input.qzonal == 'No'",
+                           condition = "input.qzonal == 'Local'",
                            selectInput("selectInput4",
                                        "Top:",choices = seq(1:84),
                                        selected =20
@@ -79,6 +95,11 @@ fluidPage(theme = shinytheme("united"),
                        # Main panel for displaying outputs ----
                        mainPanel(
                          # Output
+                         conditionalPanel(
+                           condition = "input.qzonal == 'Zonal'",
+                           plotOutput(outputId = "Zone_Ranking")
+                           ),
+                         tags$hr(),
                          plotOutput(outputId = "distPlot3")
                          
                        )
